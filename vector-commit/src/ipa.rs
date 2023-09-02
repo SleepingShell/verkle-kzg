82a31af3e5a98056560d48b17be8affaead5e067
SleepingShell <SleepingShell@users.noreply.github.com>
2023-09-01 22:18:53 -0500

    wip ipa refactor

--- vector-commit/src/ipa.rs
+++ vector-commit/src/ipa.rs
@@ -1,30 +1,130 @@
 use std::{
     collections::HashMap,
+    error::Error,
+    fmt::Display,
     iter::Sum,
     marker::PhantomData,
     ops::{Add, Mul},
 };
 
 use ark_ec::Group;
-use ark_ff::{field_hashers::HashToField, FftField, Field, One, Zero};
+use ark_ff::{
+    batch_inversion_and_mul, field_hashers::HashToField, FftField, Field, One, PrimeField, Zero,
+};
 use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial, Polynomial};
 use ark_serialize::CanonicalSerialize;
 
 use crate::{VCPreparedData, VCUniversalParams, VectorCommitment};
 
-struct IPAUniversalParams<G: Group, D: HashToField<G::ScalarField>> {
-    g: Vec<G>, // Gens to commit the evaluations of the dataset
+/// Precomputed barycentric elements in a domain of size `N`.
+struct PrecomputedLagrange<const N: usize, F: Field> {
+    // l(x) = ∏(X-x_i)
+    vanishing_polynomial: DensePolynomial<F>,
+
+    // ∑ l(x)/(X-x_i)
+    vanishing_derivative: DensePolynomial<F>,
+
+    // w_j = ∏_{m!=j}(x_j-x_m)^-1
+    barycentric_weights: [F; N],
+}
+
+impl<const N: usize, F: FftField> PrecomputedLagrange<N, F> {
+    fn new() -> Self {
+        let domain = (0..N as u64).map(|x| F::from(x)).collect::<Vec<F>>();
+        let vanishing = Self::compute_vanishing_polynomial(&domain);
+        let derivative = Self::compute_vanishing_derivative(&domain, &vanishing);
+        let weights = Self::compute_barycentric_weights(domain);
+
+        Self {
+            vanishing_polynomial: vanishing,
+            vanishing_derivative: derivative,
+            barycentric_weights: weights,
+        }
+    }
+
+    fn compute_vanishing_polynomial(x_points: &Vec<F>) -> DensePolynomial<F> {
+        let mut res = DensePolynomial::<F>::from_coefficients_slice(&[F::one()]);
+        x_points.into_iter().for_each(|x| {
+            res = &res * &DensePolynomial::<F>::from_coefficients_slice(&[F::zero() - x, F::one()]);
+        });
+
+        res
+    }
+
+    fn compute_vanishing_derivative(
+        x_points: &Vec<F>,
+        vanishing_roots: &DensePolynomial<F>,
+    ) -> DensePolynomial<F> {
+        let mut res = DensePolynomial::<F>::from_coefficients_slice(&[F::zero()]);
+        x_points.into_iter().for_each(|x| {
+            res += &(vanishing_roots
+                / &DensePolynomial::<F>::from_coefficients_slice(&[F::zero() - x, F::one()]));
+        });
+
+        res
+    }
+
+    fn compute_barycentric_weights(points: Vec<F>) -> [F; N] {
+        let mut weights = [F::zero(); N];
+        points.iter().enumerate().for_each(|(i, x)| {
+            let mut sum = F::one();
+            points.iter().for_each(|x_inner| {
+                if (x != x_inner) {
+                    sum *= *x - *x_inner;
+                }
+            });
+            weights[i] = sum.inverse().unwrap();
+        });
+
+        weights
+    }
+
+    /// Computes the b vector in IPA. When this vector is inner product'd by the evaluations in the domain,
+    /// the result is F(point)
+    fn compute_barycentric_coefficients(&self, point: F) -> Vec<F> {
+        if point < F::from(N as u64) {
+            let mut res = vec![F::zero(); N];
+            //res[point as usize] = F::one();
+            return res;
+        }
+        let vanishing_eval = self.vanishing_polynomial.evaluate(&point);
+        //let mut res_poly = DensePolynomial::<F>::from_coefficients_slice(&[F::zero()]);
+        let mut coeffs = Vec::new();
+        (0..N as u64).into_iter().for_each(|i| {
+            let f_i = F::from(i);
+            coeffs.push(self.vanishing_derivative.evaluate(&f_i) * (point - f_i));
+        });
+
+        batch_inversion_and_mul(&mut coeffs, &vanishing_eval);
+
+        coeffs
+    }
+}
+
+struct IPAUniversalParams<const N: usize, G: Group, D: HashToField<G::ScalarField>> {
+    g: [G; N], // Gens to commit the evaluations of the dataset
     q: G,      // Gen to commit to the inner product of the dataset with it's b vector
+    precompute: PrecomputedLagrange<N, G::ScalarField>,
+
     digest: PhantomData<D>,
 }
 
-impl<G: Group, D: HashToField<G::ScalarField>> IPAUniversalParams<G, D> {
-    fn new(g: Vec<G>, q: G) -> Self {
+impl<const N: usize, G: Group, D: HashToField<G::ScalarField>> IPAUniversalParams<N, G, D> {
+    fn new(g: [G; N], q: G) -> Self {
         Self {
             g,
             q,
+            precompute: PrecomputedLagrange::new(),
             digest: PhantomData,
         }
+    }
+}
+
+impl<const N: usize, G: Group, D: HashToField<G::ScalarField>> VCUniversalParams
+    for IPAUniversalParams<N, G, D>
+{
+    fn max_size(&self) -> usize {
+        N
     }
 }
 
@@ -45,94 +145,51 @@ struct IPAProof<G: Group> {
     y: G::ScalarField,
 }
 
-struct IPAPreparedData<F: Field> {
-    // An ordered set of data, of (x, eval)
-    data: Vec<(usize, F)>,
+#[derive(Clone, Debug)]
+enum IPAError {
+    OutOfDomain,
+    OutOfCRS,
+}
 
-    // We keep the weights for barycentric interpolation w_j = ∏_{m != j}(x_j - x_m)^-1
-    weights: Vec<F>,
+impl Display for IPAError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", "")
+    }
+}
 
-    // TODO: Move outside of this so we only store once l(x) = ∏(X-x_i)
-    universal_basis: DensePolynomial<F>,
+impl Error for IPAError {}
 
-    // Derivative of universal basis, ∑ l(x)/(X-x_i)
-    universal_basis_derivative: DensePolynomial<F>,
+struct IPAPreparedData<const N: usize, F: Field> {
+    data: [F; N],
 
     // The highest x value that this dataset contains
     max: usize,
 }
 
-impl<F: FftField> IPAPreparedData<F> {
+impl<const N: usize, F: FftField> IPAPreparedData<N, F> {
     /// An ORDERED list of evaluation points
-    fn new(data: Vec<(usize, F)>) -> Self {
-        let max = data[data.len() - 1].0;
-        let mut d_calc = Vec::<(F, F)>::new();
-        let mut x_points = Vec::<F>::new();
-        data.iter().for_each(|point| {
-            x_points.push(F::from(point.0 as u64));
-            d_calc.push((F::from(point.0 as u64), point.1));
-        });
-        let basis = Self::compute_universal_basis(x_points.clone());
-        Self {
-            data,
-            weights: Self::compute_barycentric_weights(d_calc),
-            universal_basis: basis.clone(),
-            universal_basis_derivative: Self::compute_universal_basis_derivative(x_points, basis),
-            max,
-        }
+    fn new(data_list: Vec<(usize, F)>) -> Self {
+        let max = data_list[data_list.len() - 1].0;
+        let mut data = [F::zero(); N];
+        data_list.into_iter().for_each(|(i, x)| data[i] = x);
+        Self { data, max }
     }
 
     /// The data domain is simply at 0..length-1
-    fn new_incremental(data: Vec<F>) -> Self {
-        let max = data.len();
-        let mut data_stored = Vec::<(usize, F)>::new();
-        data.into_iter().enumerate().for_each(|(i, v)| {
-            data_stored.push((i, v));
-        });
-
-        Self::new(data_stored)
+    fn new_incremental(data_list: Vec<F>) -> Self {
+        let max = data_list.len() - 1;
+        let mut data = [F::zero(); N];
+        data_list
+            .into_iter()
+            .enumerate()
+            .for_each(|(i, x)| data[i] = x);
+        Self { data, max }
     }
 
-    fn compute_universal_basis(x_points: Vec<F>) -> DensePolynomial<F> {
-        let mut res = DensePolynomial::<F>::from_coefficients_slice(&[F::one()]);
-        x_points.into_iter().for_each(|x| {
-            res = &res * &DensePolynomial::<F>::from_coefficients_slice(&[F::zero() - x, F::one()]);
-        });
-
-        res
-    }
-
-    fn compute_universal_basis_derivative(
-        x_points: Vec<F>,
-        universal_basis: DensePolynomial<F>,
-    ) -> DensePolynomial<F> {
-        let mut res = DensePolynomial::<F>::from_coefficients_slice(&[F::zero()]);
-        x_points.into_iter().for_each(|x| {
-            res += &(&universal_basis
-                / &DensePolynomial::<F>::from_coefficients_slice(&[F::zero() - x, F::one()]));
-        });
-
-        res
-    }
-
-    fn compute_barycentric_weights(points: Vec<(F, F)>) -> Vec<F> {
-        let mut weights: Vec<F> = Vec::new();
-        points.iter().for_each(|point| {
-            let mut sum = F::one();
-            points.iter().for_each(|p_inner| {
-                if point.0 != p_inner.0 {
-                    sum *= point.0 - p_inner.0;
-                }
-            });
-            weights.push(sum.inverse().unwrap());
-        });
-
-        weights
-    }
-
-    fn compute_b_vector(&self, index: usize) -> Vec<F> {
+    /*
+    fn compute_b_vector(&self, index: usize, precompute: &PrecomputedLagrange<N, F>) -> Vec<F> {
         let index_f = F::from(index as u64);
-        let universal_eval = self.universal_basis.evaluate(&index_f);
+        let universal_eval = precompute.vanishing_polynomial.evaluate(&index_f);
         let mut res = Vec::new();
         self.data.iter().for_each(|(x, _y)| {
             if index == *x {
@@ -140,30 +197,211 @@ impl<F: FftField> IPAPreparedData<F> {
             } else {
                 let x_f = F::from(*x as u64);
                 let rhs = index_f - x_f;
-                let deriv_eval: F = self.universal_basis_derivative.evaluate(&x_f);
+                let deriv_eval: F = precompute.vanishing_derivative.evaluate(&x_f);
                 res.push(universal_eval / (deriv_eval * rhs));
             }
         });
 
         res
     }
+    */
+}
 
-    /// Return the data of size `max` with zero entries included
-    fn expanded_data(&self) -> Vec<F> {
-        let mut res = vec![F::zero(); self.max + 1];
-        for point in self.data.iter() {
-            res[point.0] = point.1;
-        }
+impl<const N: usize, F: FftField> VCPreparedData for IPAPreparedData<N, F> {
+    type Item = F;
+    type Error = IPAError;
 
-        res
+    fn from_vec(data: Vec<Self::Item>) -> Self {
+        Self::new_incremental(data)
     }
 
-    /// Simply return the stored evaluations without their index's (densely packed)
-    fn evaluations(&self) -> Vec<F> {
-        self.data.iter().map(|(_i, p)| *p).collect()
+    fn get(&self, index: usize) -> Option<&Self::Item> {
+        if index < self.max {
+            Some(&self.data[index])
+        } else {
+            None
+        }
+    }
+
+    fn get_all(&self) -> Vec<(usize, Self::Item)> {
+        self.data
+            .into_iter()
+            .enumerate()
+            .filter(|(i, x)| *x != F::zero())
+            .collect()
+    }
+
+    fn max_size(&self) -> usize {
+        N
+    }
+
+    fn set_evaluation(&mut self, index: usize, value: Self::Item) -> Result<(), Self::Error> {
+        if index > N {
+            Err(IPAError::OutOfDomain)
+        } else {
+            self.data[index] = value;
+            Ok(())
+        }
     }
 }
 
+struct IPA<const N: usize, G, D> {
+    _g: PhantomData<G>,
+    _d: PhantomData<D>,
+}
+
+impl<const N: usize, G, D> VectorCommitment for IPA<N, G, D>
+where
+    G: Group,
+    D: HashToField<G::ScalarField>,
+{
+    type UniversalParams = IPAUniversalParams<N, G, D>;
+    type PreparedData = IPAPreparedData<N, G::ScalarField>;
+    type Commitment = IPACommitment<G>;
+    type Proof = IPAProof<G>;
+    type BatchProof = Vec<Self::Proof>;
+    type Error = IPAError;
+
+    fn setup<R: rand::RngCore>(
+        max_items: usize,
+        rng: &mut R,
+    ) -> Result<Self::UniversalParams, Self::Error> {
+        todo!()
+    }
+
+    fn commit(
+        key: &Self::UniversalParams,
+        data: &Self::PreparedData,
+    ) -> Result<Self::Commitment, Self::Error> {
+        Ok(inner_product(&key.g, &data.data))
+    }
+
+    fn convert_commitment_to_data(
+        commit: &Self::Commitment,
+    ) -> <Self::PreparedData as VCPreparedData>::Item {
+        if commit.is_zero() {
+            G::ScalarField::ZERO
+        } else {
+            let mut bytes = Vec::new();
+            //TODO: Check
+            commit.serialize_compressed(&mut bytes).unwrap();
+            G::ScalarField::from_le_bytes_mod_order(&bytes)
+        }
+    }
+
+    fn prove(
+        key: &Self::UniversalParams,
+        commitment: &Self::Commitment,
+        index: usize,
+        data: &Self::PreparedData,
+    ) -> Result<Self::Proof, Self::Error> {
+        todo!()
+    }
+
+    fn prove_all(
+        key: &Self::UniversalParams,
+        commitment: &Self::Commitment,
+        data: &Self::PreparedData,
+    ) -> Result<Self::BatchProof, Self::Error> {
+        todo!()
+    }
+
+    fn verify(
+        key: &Self::UniversalParams,
+        commitment: &Self::Commitment,
+        proof: &Self::Proof,
+    ) -> Result<bool, Self::Error> {
+        todo!()
+    }
+
+    fn verify_batch(
+        key: &Self::UniversalParams,
+        commitment: &Self::Commitment,
+        proof: &Self::BatchProof,
+    ) -> Result<bool, Self::Error> {
+        todo!()
+    }
+}
+impl<const N: usize, G, D> IPA<N, G, D>
+where
+    G: Group,
+    D: HashToField<G::ScalarField>,
+{
+    /// Prove that we have made a valid commitment
+    pub fn prove_commitment(
+        key: &IPAUniversalParams<N, G, D>,
+        commitment: &IPACommitment<G>,
+        data: &IPAPreparedData<N, G::ScalarField>,
+    ) -> IPACommitProof<G> {
+        let max = data.max;
+        let mut data = data.data[0..max + 1].to_vec();
+        let mut gens = key.g[0..max + 1].to_vec();
+        let mut l = Vec::<G>::new();
+        let mut r = Vec::<G>::new();
+
+        let hasher = D::new(&[]);
+        let mut ra = hasher.hash_to_field(&serialize(commitment), 1)[0];
+
+        while data.len() > 1 {
+            let (data_l, data_r) = split(data);
+            let (gens_l, gens_r) = split(gens);
+
+            let y_l = inner_product(&gens_r, &data_l);
+            let y_r = inner_product(&gens_l, &data_r);
+            l.push(y_l);
+            r.push(y_r);
+
+            ra = hasher.hash_to_field(
+                &[serialize(&ra), serialize(&y_l), serialize(&y_r)].concat(),
+                1,
+            )[0];
+
+            data = vec_add_and_distribute(&data_l, &data_r, ra);
+            gens = vec_add_and_distribute(&gens_r, &gens_l, ra);
+        }
+        IPACommitProof {
+            l: l,
+            r: r,
+            tip: data[0],
+        }
+    }
+
+    /// Verify that a commitment is valid
+    pub fn verify_commitment_proof(
+        key: &IPAUniversalParams<N, G, D>,
+        commitment: &IPACommitment<G>,
+        proof: &IPACommitProof<G>,
+    ) -> bool {
+        let gens = key.g[0..(2usize).pow(proof.l.len() as u32)].to_vec();
+        let mut c = commitment.clone();
+        let hasher = D::new(&[]);
+        let mut ra = hasher.hash_to_field(&serialize(commitment), 1)[0];
+        let mut points_coeffs = vec![G::ScalarField::one()];
+
+        for i in 0..proof.l.len() {
+            ra = hasher.hash_to_field(
+                &[
+                    serialize(&ra),
+                    serialize(&proof.l[i]),
+                    serialize(&proof.r[i]),
+                ]
+                .concat(),
+                1,
+            )[0];
+
+            c = proof.l[i] + c * ra + proof.r[i] * ra.square();
+            points_coeffs = points_coeffs
+                .into_iter()
+                .map(|x| vec![x * ra, x])
+                .flatten()
+                .collect();
+        }
+
+        let combined_point = inner_product(&gens, &points_coeffs);
+        c == combined_point * proof.tip
+    }
+}
+/*
 /// Commit to the dataset
 fn commit_data<G: Group, D: HashToField<G::ScalarField>>(
     key: &IPAUniversalParams<G, D>,
@@ -334,6 +572,7 @@ fn verify_eval<G: Group, D: HashToField<G::ScalarField>>(
 
     c == combined_point * proof.tip + q * (proof.tip * combined_x_pow)
 }
+*/
 
 fn serialize<T: CanonicalSerialize>(x: &T) -> Vec<u8> {
     let mut b = Vec::new();
@@ -379,25 +618,30 @@ mod tests {
     type G = <Bn254 as Pairing>::G1;
     type Hasher = DefaultFieldHasher<Sha256>;
 
+    const SIZE: usize = 32;
+    type IPAT = IPA<SIZE, G, Hasher>;
+
     #[test]
     fn test_commit_poly() {
-        let size = 32;
-        let gens: Vec<G> = (0..size).map(|i| G::generator() * F::from(i + 1)).collect();
-        let q = G::generator() * F::from(size + 1);
+        let mut gens = [G::zero(); SIZE];
+        (0..SIZE).for_each(|i| gens[i] = G::generator() * F::from(i as u64 + 1));
+        let q = G::generator() * F::from(SIZE as u64 + 1);
 
-        let data_raw: Vec<F> = (0..size).map(|i| F::from(i)).collect();
+        let data_raw: Vec<F> = (0..SIZE as u64).map(|i| F::from(i)).collect();
 
-        let crs = IPAUniversalParams::<G, Hasher>::new(gens, q);
-        let data = IPAPreparedData::<F>::new_incremental(data_raw);
+        let crs = IPAUniversalParams::<SIZE, G, Hasher>::new(gens, q);
+        let data = IPAPreparedData::<SIZE, F>::new_incremental(data_raw);
 
-        let mut commit = commit_data(&crs, &data);
-        let poly_proof = prove_data_commit(&crs, &commit, &data);
-        assert!(verify_data_commit(&crs, &commit, &poly_proof));
+        let mut commit = IPAT::commit(&crs, &data).unwrap();
+
+        let proof = IPAT::prove_commitment(&crs, &commit, &data);
+        assert!(IPAT::verify_commitment_proof(&crs, &commit, &proof));
 
         commit += G::generator();
-        assert!(!verify_data_commit(&crs, &commit, &poly_proof));
+        assert!(!IPAT::verify_commitment_proof(&crs, &commit, &proof));
     }
 
+    /*
     #[test]
     fn test_eval_proof() {
         let size: u64 = 32;
@@ -414,4 +658,5 @@ mod tests {
         let e_proof = prove_eval(&crs, &commit, index, &data);
         assert!(verify_eval(&crs, &commit, &e_proof));
     }
+    */
 }
